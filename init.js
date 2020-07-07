ebe77759c86b91a1c7865517395f22e58c5af05d
machete <rtpppp163@gmail.com>
2020-07-07 15:57:59 +0800

    init 添加询问与参数

--- init.js
+++ init.js
@@ -3,43 +3,89 @@
 /**
  * 请在工程根目录下执行：node init.js
  */
-// TODO  可选antd  element vant eapp
-const type = 'antd'
 const _execSync = require('child_process').execSync
 const execSync = (val) => { _execSync(val, { stdio: 'inherit' }) }
 const fs = require('fs')
 const path = require('path')
-
 console.log('正在安装初始化脚本依赖，请稍后...')
-execSync('npm install yeoman-environment generator-yo-template shelljs')
-console.log('安装初始化脚本依赖完成')
-const yeoman = require('yeoman-environment')
-const shelljs = require('shelljs')
-const yo_env = yeoman.createEnv()
+// 最小的两个包
+execSync('npm install inquirer optimist')
+const argv = require('optimist').argv;
+const inquirer = require('inquirer')
 const name = 'generator'
-console.log('安装项目模板')
+const types = ['antd','element','eapp']
+const tools = ['npm','cnpm','yarn']
+let tool = ''
+async function chooseType(){
+  const { type } = await inquirer.prompt([ { 
+    type: 'list', 
+    name:'type',
+    choices: types,
+    message: 'what template would you like use?', 
+    default: true 
+  }])
+  return type
+}
 
-yo_env.register(`generator-yo-template/generators/${type}/index.js`, name)
-yo_env.run(`${name}`, {}, function (err) {
-  try {
-    console.log('安装项目模板完成')
-    // shelljs.rm('-rf init.js')
-    shelljs.rm('-rf', 'node_modules')
-    shelljs.rm('-rf', '*.lock')
-    shelljs.mv('-f', 'gitignore', '.gitignore')
-    console.log('清理初始化脚本成功，开始自动安装npm依赖')
-    // install()
-  } catch (e) {
-    console.log('清理初始化脚本失败')
+async function chooseManagerTool(){
+  const { tool } = await inquirer.prompt([ { 
+    type: 'list', 
+    name: 'tool',
+    choices: tools,
+    message: 'what tool would you like use?', 
+    default: true 
+  }])
+  return tool
+}
+
+async function run(){
+  const type = types.includes(argv.t) ? argv.t : await chooseType()
+  tool = tools.includes(argv.u) ? argv.u : await chooseManagerTool()
+  console.log('正在安装环境依赖')
+  const packList = 'yeoman-environment generator-yo-template shelljs'
+  if(tool === 'yarn'){
+    execSync(`yarn add ${packList}`)
+  }else{
+    execSync(`${tool} install ${packList}`)
   }
-  if (err) {
-    console.log(err)
-  }
-})
+  const yeoman = require('yeoman-environment')
+  const shelljs = require('shelljs')
+  const yo_env = yeoman.createEnv()
+  console.log('安装初始化脚本依赖完成')
+  console.log('安装项目模板')
+  shelljs.rm('-rf','*')
+  shelljs.rm('-rf','.*')
+  yo_env.register(`generator-yo-template/generators/${type}/index.js`, name)
+  yo_env.run(`${name}`, {}, function (err) {
+    try {
+      console.log('安装项目模板完成')
+      shelljs.rm('-rf','init.js')
+      shelljs.rm('-rf', 'node_modules')
+      shelljs.rm('-rf', '*.lock')
+      shelljs.mv('-f', 'gitignore', '.gitignore')
+      console.log('清理初始化脚本成功')
+      install()
+    } catch (e) {
+      console.log('清理初始化脚本失败')
+    }
+    if (err) {
+      console.log(err)
+    }
+  })
+  
+}
+
 
 function install () {
-  const pkg = path.join(__dirname, 'package.json')
-  if (fs.existsSync(pkg)) {
-    execSync('npm install', { stdio: 'inherit' })
+  if(argv.a){
+    console.log('开始自动安装项目依赖')
+    const pkg = path.join(__dirname, 'package.json')
+    if (fs.existsSync(pkg)) {
+      execSync('npm install', { stdio: 'inherit' })
+    }
   }
 }
+
+
+
+run()
